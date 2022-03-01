17d5e93599dee5c0da95f5553771a2a6fafc5848
Raju14321 <100340131+Raju14321@users.noreply.github.com>
2022-03-01 09:31:05 +0530

    Add files via upload

---
+++ inout parameters.css/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace inout_parameters.css
+{
+    class InOutparameters
+    {
+        public void Factorial(ref uint fact)
+        {
+            if (fact == 0 || fact == 1)
+            {
+                fact = 1;
+            }
+            else
+            {
+                uint result = 1;
+                for (uint i = 2; i <= fact; i++)
+                {
+                    result = result * i;
+                }
+                fact = result;
+            }
+        }
+        static void Main()
+        {
+            InOutparameters p = new InOutparameters();
+            uint num = 5;
+            Console.Write($"Factorial of {num} is: ");
+            p.Factorial(ref num);
+            Console.WriteLine(num);
+            Console.ReadLine();
+        }
+    }
+}
